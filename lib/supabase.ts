d86e5f2cbb39cd18e7ea581fd2e60adaf0c1f584
nategq <nathangilgar@gmail.com>
2025-12-17 14:10:18 -0500

    Initialize Supabase client in supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "YOUR_SUPABASE_PROJECT_URL";
+const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
